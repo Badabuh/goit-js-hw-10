40e39e813da04b48bda9e0c2f3eb50aa4498f4dc
Petrovskyi <ivan.prog74@gmail.com>
2026-03-14 12:30:09 +0100

    fix

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,12 +1,35 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const endingTime = document.querySelector("#datetime-picker");
-const startBtn = document.querySelector("button[data-start]");
+const endingTime = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
 
 let selectedTime = null;
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 const options = {
   enableTime: true,
@@ -17,35 +40,46 @@ const options = {
     console.log(selectedDates[0]);
     selectedTime = selectedDates[0].getTime();
     if (selectedTime < Date.now()) {
-        iziToast.error({
-            title: "Error",
-            message: "Please choose a date in the future",
-            position: "topRight",
-        });
-    }else{
-        startBtn.disabled = false;
-        startBtn.style.color = "black";
-        startBtn.style.border = "2px solid black";
-        startBtn.addEventListener("click", () => {
-            const timerId = setInterval(() => {
-                const currentTime = Date.now();
-                const deltaTime = selectedTime - currentTime;
-                if (deltaTime <= 0) {
-                    clearInterval(timerId);
-                    return;
-                }
-                document.querySelector(".value[data-days]").textContent = String(Math.floor(deltaTime / (1000 * 60 * 60 * 24))).padStart(2, "0");
-                document.querySelector(".value[data-hours]").textContent = String(Math.floor((deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
-                document.querySelector(".value[data-minutes]").textContent = String(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
-                document.querySelector(".value[data-seconds]").textContent = String(Math.floor((deltaTime % (1000 * 60)) / 1000)).padStart(2, "0");
-                startBtn.disabled = true;
-                startBtn.style.color = "#b8b8b8";
-                startBtn.style.border = "2px solid #c6c6c6";
-                endingTime.disabled = true;
-        }, 1000);
-        });
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      return;
+    } else {
+      startBtn.disabled = false;
+      startBtn.style.color = 'black';
+      startBtn.style.border = '2px solid black';
     }
   },
 };
+startBtn.addEventListener('click', () => {
+  const timerId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = selectedTime - currentTime;
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      selectedTime = null;
+      startBtn.disabled = false;
+      startBtn.style.color = 'black';
+      startBtn.style.border = '2px solid black';
+      endingTime.disabled = false;
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    document.querySelector('.value[data-days]').textContent =
+      addLeadingZero(days);
+    document.querySelector('.value[data-hours]').textContent =
+      addLeadingZero(hours);
+    document.querySelector('.value[data-minutes]').textContent =
+      addLeadingZero(minutes);
+    document.querySelector('.value[data-seconds]').textContent =
+      addLeadingZero(seconds);
+    startBtn.disabled = true;
+    startBtn.style.color = '#b8b8b8';
+    startBtn.style.border = '2px solid #c6c6c6';
+    endingTime.disabled = true;
+  }, 1000);
+});
 
 flatpickr(endingTime, options);
